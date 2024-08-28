abecb3691f64084ded2290e136ea280ca6797d6f
Silas <102931703+Cyrax2350@users.noreply.github.com>
2024-08-28 10:48:04 +1000

    Add files via upload

---
+++ CIBO_cadastral_SD.R
@@ -0,0 +1,100 @@
+#portfolio_cadastral 
+#--------------------------------------------------------------------------------
+# Project geometry processing
+#--------------------------------------------------------------------------------
+# SETUP
+#--------------------------------------------------------------------------------
+# set working directory
+setwd("/Users/silas.darnell/Dropbox (GreenCollar)/R Apps Development/R Apps Research/Silas/Spatial_projects")
+
+#--------------------------------------------------------------------------------
+Sys.setenv(TZ='Australia/Sydney')
+# get current date
+current_date=Sys.Date()
+current_datetime=Sys.time()
+#--------------------------------------------------------------------------------
+#install.packages("geojson")
+library(dplyr) 
+library(lubridate) 
+library(sf) 
+library(RMariaDB)
+library(mapboxapi) 
+library(geojson)
+library(ggplot2)
+sf_use_s2(TRUE)
+message(sf_use_s2())
+sessionInfo()
+#--------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------
+settingsfile = '~/Documents/silas.cnf' 
+
+all_projectDb <- dbConnect(RMariaDB::MariaDB(), default.file = settingsfile, group="mysql-prod", dbname = 'erf_project')  
+dbListTables(all_projectDb)
+
+
+dbListTables(all_projectDb) 
+
+querya="SELECT g.project_id,p.project_name,p.portfolio,ST_AsText(g.cadastral_boundary) As geometry,ST_SRID(g.cadastral_boundary) AS geometry_epsg, AsWKT(ST_Centroid(g.cadastral_boundary)) as centroid
+FROM erf_project.project_geometry g
+right JOIN erf_project.all_project p
+ON p.project_id = g.project_id
+WHERE p.project_id IN(617823,610400,610100,600200,617828,604400,614000,607600,601500,617812,606400,613700,618289,613900,618223,602800,618237,601700,601300,
+  603100,604700,600900,617804,617805,610300,618164,602400,613300,609000,617923,609900,617802,617845,617789,601000,612100,609600,615000,601400,602500,604100,
+    601900,602200,601200,603000,604900,617850,605300,617929,602000)
+and g.cadastral_boundary is not null;"
+
+print(querya)
+a = dbSendQuery(all_projectDb,querya)
+project_geometry = dbFetch(a)
+print(a)
+
+dbClearResult(a)
+
+dbDisconnect(all_projectDb)
+#--------------------------------------------------------------------------------
+# Inputs processing
+#--------------------------------------------------------------------------------
+# cadastral boundaries processing
+#--------------------------------------------------------------------------------
+sfc= st_sfc()
+files.project = c(project_geometry$project_id)
+project_geometry_simp = project_geometry
+start.loop = Sys.time()
+
+for (f.project in files.project){
+  
+  message(f.project)
+  pos.project = which(project_geometry$project_id==f.project)
+  project_name = project_geometry$project_name[pos.project]
+  project_id = as.integer(project_geometry$project_id[pos.project])
+  portfolio = as.character(project_geometry$portfolio[pos.project])
+  
+  boundary_in = st_as_sfc(project_geometry$geometry[pos.project])
+  st_crs(boundary_in) = project_geometry$geometry_epsg[pos.project]
+  
+  boundary_in_GDA94 = st_transform(boundary_in,4283)
+  boundary_in_GDA94 = st_make_valid(boundary_in_GDA94)
+  
+  boundary_in_GDA94 = st_sfc(boundary_in_GDA94)
+  boundary_in_GDA94 = st_sf(data.frame(geom=boundary_in_GDA94,project_id, project_name, portfolio))
+  
+  length(sfc)
+  if((length(sfc))==0){sfc=boundary_in_GDA94} else
+  {sfc = do.call(rbind, list(sfc, boundary_in_GDA94))}
+  
+}
+
+end.loop = Sys.time()
+message("inputs processing (cadastral boundaries) duration: ", difftime(end.loop,start.loop, units = "mins")," minutes")
+
+if(file.exists('output/CIBO_projects.geojson')) {file.remove('output/CIBO_projects.geojson')}
+st_write(sfc, "output/CIBO_projects.geojson")
+
+if(file.exists('output/CIBO_projects.kml')) {file.remove('output/CIBO_projects.kml')}
+st_write(sfc, "output/CIBO_projects.kml")
+
+if(file.exists('output/CIBO_projects.shp')) {file.remove('output/CIBO_projects.shp')}
+st_write(sfc, "output/CIBO_projects.shp")
+
+
+
